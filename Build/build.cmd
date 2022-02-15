a617b14318329419fabb8c3f7f6cf2dbebaa952c
aafonenko@ALADDIN <aafonenko@ALADDIN@a2bdab5c-7d7b-994d-8ad2-96517821a9b1>
2022-02-15 13:08:02 +0000

    git-svn-id: https://svn-sccapi.aladdin.ru/svn/SCCAPI.8/trunk@18 a2bdab5c-7d7b-994d-8ad2-96517821a9b1

--- Build/build.cmd
+++ Build/build.cmd
@@ -1,3 +1,4 @@
 set msbuild_dir=C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\amd64
-"%msbuild_dir%\msbuild.exe" SCCAPI.NET.Core.sln /t:restore 
-"%msbuild_dir%\msbuild.exe" build.all.xml /toolsversion:4.0 /t:Build -restore /p:VisualStudioVersion=16.0 /clp:PerformanceSummary;Summary;Verbosity=diag
+rem "%msbuild_dir%\msbuild.exe" SCCAPI.NET.Core.sln /t:restore 
+rem "%msbuild_dir%\msbuild.exe" build.all.xml /toolsversion:4.0 /t:Build -restore /p:VisualStudioVersion=16.0 /clp:PerformanceSummary;Summary;Verbosity=diag
+"%msbuild_dir%\msbuild.exe" build.all.xml /t:Build /p:VisualStudioVersion=16.0 /clp:PerformanceSummary;Summary;Verbosity=diag
