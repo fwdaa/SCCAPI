f0e17288420f2985a6dc96daba485fe82e6a0436
aafonenko@ALADDIN <aafonenko@ALADDIN@a2bdab5c-7d7b-994d-8ad2-96517821a9b1>
2023-11-16 13:20:14 +0000

    git-svn-id: https://svn-sccapi.aladdin.ru/svn/SCCAPI.8/trunk@174 a2bdab5c-7d7b-994d-8ad2-96517821a9b1

--- Trace/TraceWinNT.h
+++ Trace/TraceWinNT.h
@@ -2,13 +2,36 @@
 #include "ntapi.h"
 
 ///////////////////////////////////////////////////////////////////////////////
+// Описание ошибки Native API
+///////////////////////////////////////////////////////////////////////////////
+#if !defined _NTDDK_
+inline DWORD WINERROR_FROM_NTSTATUS(NTSTATUS status)
+{
+    // проверить наличие ошибки
+    if (!FAILED(status)) return ERROR_SUCCESS; 
+
+    // указать прототип функции
+	typedef DWORD (WINAPI* PfnRtlNtStatusToDosError)(NTSTATUS);
+
+	// определить адрес модуля
+    HMODULE hModule = ::GetModuleHandleW(L"ntdll.dll");
+
+	// получить адрес функции
+	FARPROC pfn = ::GetProcAddress(hModule, "RtlNtStatusToDosError");
+																			
+	// преобразовать код ошибки 
+	return (*(PfnRtlNtStatusToDosError)pfn)(status);
+}
+#endif 
+
+///////////////////////////////////////////////////////////////////////////////
 // Трассировка ошибок NTSTATUS
 ///////////////////////////////////////////////////////////////////////////////
 #if defined _MANAGED && _MANAGED == 1
 #define WPP_TRACELEVEL_NTSTATUS_RAISE(FILE, LINE)                           \
     windows_error(HRESULT_FROM_NT(WPP_VAR(LINE))).trace(FILE, LINE);        \
     throw gcnew System::ComponentModel::Win32Exception(                     \
-        HRESULT_FROM_WIN32(RtlNtStatusToDosError(WPP_VAR(LINE))).value()    \
+        HRESULT_FROM_WIN32(WINERROR_FROM_NTSTATUS(WPP_VAR(LINE))).value()    \
     );  
 #elif !defined _NTDDK_
 #define WPP_TRACELEVEL_NTSTATUS_RAISE(FILE, LINE)                           \
